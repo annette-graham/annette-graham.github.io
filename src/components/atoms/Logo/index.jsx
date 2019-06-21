24ba4ee58158a29255e144fc5b44c2e9e0b90041
Annette Graham <annettegraham56@gmail.com>
2019-06-21 08:40:15 +0100

    created logo

--- src/components/atoms/Logo/index.jsx
+++ src/components/atoms/Logo/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from "gatsby"
+
 import styles from './logo.module.css'
 
 import sloth from '../../../images/yoga-sloth.png'
@@ -6,9 +8,14 @@ import sloth from '../../../images/yoga-sloth.png'
 const Logo = ({ siteTitle }) => {
 
   return (
-    <div>
-      <img alt="" src={sloth} className="logo"/>
-      { siteTitle }
+    <div className={styles.logo}>
+      <Link
+        className={styles.logoLink}
+        to="/"
+      >
+        <img alt="" src={sloth} className={styles.logoImage}/>
+        <span className={styles.logoText}>A.G</span>
+      </Link>
     </div>
   )
 
