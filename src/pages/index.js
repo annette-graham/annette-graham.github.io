3d69809ffa0d57f69fea7c66469895f92d263f58
Annette Graham <annettegraham56@gmail.com>
2019-06-21 08:59:55 +0100

    Added extra lines of text

---
+++ src/pages/index.js
@@ -0,0 +1,72 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import styles from './home.module.css'
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Home = () => (
+  <Layout>
+    <SEO title="Annette Graham" keywords={[`gatsby`, `application`, `react`]} />
+    <div className={styles.body}>
+
+    </div>
+    <h1>I AM ANNETTE AND I AM A SOFTWARE ENGINEER OKAY.<br/>GOSH</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default Home
