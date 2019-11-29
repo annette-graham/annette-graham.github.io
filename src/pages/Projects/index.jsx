64e2d697c5ee4d1b05da5e0012f791b2d1e24b6d
Annette Graham <annettegraham56@gmail.com>
2019-11-29 13:56:46 +0000

    added pages

---
+++ src/pages/Projects/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import sloth from '../../images/hanging-sloth.png'
+
+
+const Projects = () => {
+
+  return (
+
+    <section id="/projects" className='about'>
+      <div className="body">
+        <div className='body-content'>
+          <img alt="Sloth doing tree pose" src={sloth} className='logoImage' />
+          <h1 className="header-text">I GOT SWEET F-A PROJECTS</h1>
+          <p>This is my project.</p>
+          <p>Literally, what else you expect?</p>
+          <p>I got nothin else r u happy!?!?!</p>
+          <p>Stop laughing</p>
+          <p>Dick.</p>
+          <br />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+
+export default Projects
