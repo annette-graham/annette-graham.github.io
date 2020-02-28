24d8f66463f4cbc3279622b79d3ceeb862324bd5
annette-graham <annette.graham@bbc.co.uk>
2020-02-28 15:47:49 +0000

    changed displayed text

--- src/components/molecules/Form/index.jsx
+++ src/components/molecules/Form/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import sloth from '../../../images/reading-sloth2.png'
 
 import Input from '../../atoms/Input'
 
@@ -6,24 +7,27 @@ const Form = () => {
 
     return (
         <div className='contact-form'>
-          <Input
-            tag="input"
-            label="Name"
-            name="Name"
-            placeholder="Your name"
-          />
-          <Input
-            tag="input"
-            label="Email"
-            name="Email"
-            placeholder="example@email.com"
-          />
-          <Input
-            tag="textarea"
-            label="Message"
-            name="Message"
-            placeholder="Hey Annette, let's work together!"
-          />
+          <div className='input-group'>
+            <Input
+              tag="input"
+              label="Name"
+              name="Name"
+              placeholder="What's yo name fool?"
+            />
+            <Input
+              tag="input"
+              label="Email"
+              name="Email"
+              placeholder="example@email.com"
+            />
+            <Input
+              tag="textarea"
+              label="Message"
+              name="Message"
+              placeholder="Omg Annette U da sh!t. We must hire you. I'll pay you £100k"
+            />
+          </div>
+          <img alt="Sloth reading a piece of paper" src={sloth} className='form-image' />
         </div>
     )
 }
