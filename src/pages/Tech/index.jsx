97d845d68df8ba1b6d49bd2fc3931db63cce675c
annette-graham <annette.graham@bbc.co.uk>
2020-02-28 15:44:54 +0000

    added tech

--- src/pages/Tech/index.jsx
+++ src/pages/Tech/index.jsx
@@ -14,17 +14,52 @@ const Tech = () => {
         <Heading
           title='HERE IS THE TECH I USE'
         />
-        <p>I use tech.</p>
-        <p>I use tech.</p>
-        <p>I use tech.</p>
-        <p>I use tech.</p>
-        <p>I use tech.</p>
-        <p>I use tech.</p>
-        <p>I use tech.</p>
-        <p>I use tech.</p>
-        <p>I use tech.</p>
-        <p>I use tech.</p>
-        <br />
+        <div className='tech-container'>
+          <div className='tech-section'>
+            <h2>Languages</h2>
+            <p>ES6 Javascript</p>
+            <p>HTML</p>
+            <p>CSS</p>
+            <br />
+            <h2>Front end</h2>
+            <p>React</p>
+            <p>Redux (non-production)</p>
+            <p>Less</p>
+            <p>Sass</p>
+            <br />
+          </div>
+          <div className='tech-section'>
+            <h2>Back end</h2>
+            <p>NodeJS</p>
+            <p>Express</p>
+            <p>GraphQL (non-production)</p>
+            <p>DynamoDB</p>
+            <p>SQL</p>
+            <br />
+          </div>
+          <div className='tech-section'>
+            <h2>Cloud</h2>
+            <p>AWS Serverless</p>
+            <p>AWS EC2</p>
+            <p>AWS Lambda</p>
+            <p>AWS S3</p>
+            <br />
+            <h2>Design/UX</h2>
+            <p>Figma</p>
+          </div>
+          <div className='tech-section'>
+            <h2>Testing</h2>
+            <p>Jest</p>
+            <p>Enzyme</p>
+            <br />        
+          <h2>Tools</h2>
+            <p>Atom</p>
+            <p>VS Code</p>
+            <p>GitHub</p>
+            <p>PostMan</p>
+            <br />
+          </div>
+        </div>
       </div>
     </section>
   )
