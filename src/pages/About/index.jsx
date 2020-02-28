af2b1ae138205cefe71cfa7ed44f99b7dd5ed355
annette-graham <annette.graham@bbc.co.uk>
2020-02-28 15:46:11 +0000

    changed info displayed

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -11,31 +11,35 @@ const About = () => {
         <Heading
           title='ABOUT ME HOLY MOLY'
         />
+        <div className="portrait" id="portrait">
+          <div className="fill cover-img"></div>
+            <div className="fill z-10 hover">
+              <div className="fill cover-img hover-img"></div>
+          </div>
+        </div>
         <img alt="Sloth waving and saying hi" src={sloth} className='image hi-image' />
-        <img alt="me" src='https://picsum.photos/id/237/250/250' className='image me-image' />
+        <div>
 
-        <p>This is what I am about</p>
-        <p>This is what I am about</p>
-        <p>This is what I am about</p>
-        <p>This is what I am about</p>
-        <p>This is what I am about</p>
-        <p>This is what I am about</p>
-        <p>This is what I am about</p>
-        <p>This is what I am about</p>
-        <p>This is what I am about</p>
-        <p>This is what I am about</p>
-        <p>This is what I am about</p>
-        <p>This is what I am about</p>
-        <p>This is what I am about</p>
-        <p>This is what I am about</p>
-        <p>This is what I am about</p>
-        <p>This is what I am about</p>
-        <p>This is what I am about</p>
-        <p>This is what I am about</p>
-        <p>This is what I am about</p>
+        </div>
+        <img alt="me" src='https://picsum.photos/id/237/250/250' className='image me-image' />
+       
+        <p>This is what I'm about aite</p>
+        <br/>
+        <p>I'm Annette. I am a software engineer working at the good old BBC lol.</p>
+        <p>I did a 9 week webdev bootcamp in NZ then got this job right after.</p>
+        <p>Got promoted from junior to midweight within a year so idk guess I'm pretty dope at my job ayeee</p>
+        <br/>
+        <p>I like sloths</p>
+        <p>I'm 26</p>
+        <p>Live in London atm</p>
+        <p>Killing life</p>
+        <p>Chill af</p>
+        <p>et cetera</p>
+        <br/>
+        <p>Thas me. Any more just ask no what I'm sayin'</p>
         <br/>
         <br/>
-        <a href="/" className='anchorLink'>Go back to the homepage</a>
+        <a href="/" className='anchorLink'>Go back to the homepage fool</a>
       </div>   
     </section>
   )
