a678da19305bc4befbad9f56b37ad8baafd8c4b3
annette-graham <annette.graham@bbc.co.uk>
2020-03-20 10:32:45 +0000

    alt text on images

--- src/components/atoms/SocialMedia/index.jsx
+++ src/components/atoms/SocialMedia/index.jsx
@@ -10,19 +10,19 @@ const SocialMedia = () => {
     return (
         <div>
             <a target='_blank' rel="noopener noreferrer" href='https://www.linkedin.com/in/annette-graham-b449a115b/' className='anchorLink'>
-                <img alt="linkedin icon" src={linkedin} className='image media-image' />
+                <img alt="Yeah man I got linkedin" src={linkedin} className='image media-image' />
             </a>
             <a target='_blank' rel="noopener noreferrer" href='https://github.com/annette-graham' className='anchorLink'>
-              <img alt="github icon" src={github} className='image media-image' />
+              <img alt="Of course I got gihub. Jeez" src={github} className='image media-image' />
             </a>
             <a target='_blank' rel="noopener noreferrer" href='https://www.instagram.com/netts_spaghetts/?hl=en' className='anchorLink'>
-               <img alt="instagram icon" src={instagram} className='image media-image' />        
+               <img alt="I got insta bb" src={instagram} className='image media-image' />        
             </a>
             <a target='_blank' rel="noopener noreferrer" href='mailto:annettegraham56@gmail.com?subject=Hi%20from%20your%20portfolio!' className='anchorLink'>
-              <img alt="email icon" src={email} className='image media-image' />       
+              <img alt="I got email" src={email} className='image media-image' />       
             </a>
             <a target='_blank' rel="noopener noreferrer" href='https://codepen.io/annette-graham' className='anchorLink'>
-               <img alt="codepen icon" src={codepen} className='image media-image' />
+               <img alt="I so do got codepen" src={codepen} className='image media-image' />
             </a>            
         </div>
     )
