2567c9479a0ce74827991edb7549267c8aac2e3e
annette-graham <annette.graham@bbc.co.uk>
2020-04-28 08:17:47 +0100

    Add up arrow

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './styles/_main.scss'
+import upArrow from './images/up-arrow.svg'
 
 import Home from "./pages/Home"
 import About from "./pages/About"
@@ -11,6 +12,21 @@ import Header from "./components/molecules/Header"
 
 const App = () => {
 
+  const topArrow = document.getElementById('scroll-button')
+
+  window.onscroll = () => scrollFunction()
+
+  const scrollFunction = () => {
+    document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 
+      ? topArrow.style.display = "block"
+      : topArrow.style.display = "none"
+  }
+
+  const topFunction = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
   return (
     <div className="App">
 
@@ -20,6 +36,12 @@ const App = () => {
       <div className='main'>
         <Home />
       </div>
+      <button onclick={topFunction()} id="scroll-button" className="top-scroller js-top-scroller svg-icons" title="Go to top">
+       <img alt="Back to Top" src={upArrow} className="scroller-svg" />
+      </button>
+      {/* <a href="#top" id="scroll-button" className="top-scroller js-top-scroller svg-icons">
+        <img alt="Back to Top" src={upArrow} className="scroller-svg" />
+      </a> */}
       <div className='about'>
         <About />
       </div>
