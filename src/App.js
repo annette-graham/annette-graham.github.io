bd2954aad54e336314ca1c5bec72d59d411713fd
annette-graham <annette.graham@bbc.co.uk>
2020-07-06 10:13:39 +0100

    uncomment scroller

--- src/App.js
+++ src/App.js
@@ -12,20 +12,20 @@ import Header from "./components/molecules/Header"
 
 const App = () => {
 
-  const topArrow = document.getElementById('scroll-button')
+  // const topArrow = document.getElementById('scroll-button')
 
-  window.onscroll = () => scrollFunction()
+  // window.onscroll = () => scrollFunction()
 
-  const scrollFunction = () => {
-    document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 
-      ? topArrow.style.display = "block"
-      : topArrow.style.display = "none"
-  }
+  // const scrollFunction = () => {
+  //   document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 
+  //     ? topArrow.style.display = "block"
+  //     : topArrow.style.display = "none"
+  // }
 
-  const topFunction = () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
+  // const topFunction = () => {
+  //   document.body.scrollTop = 0;
+  //   document.documentElement.scrollTop = 0;
+  // }
 
   return (
     <div className="App">
@@ -36,12 +36,12 @@ const App = () => {
       <div className='main'>
         <Home />
       </div>
-      <button onclick={topFunction()} id="scroll-button" className="top-scroller js-top-scroller svg-icons" title="Go to top">
+      {/* <button onclick={topFunction()} id="scroll-button" className="top-scroller js-top-scroller svg-icons" title="Go to top">
        <img alt="Back to Top" src={upArrow} className="scroller-svg" />
-      </button>
-      {/* <a href="#top" id="scroll-button" className="top-scroller js-top-scroller svg-icons">
+      </button> */}
+      <a href="#top" id="scroll-button" className="top-scroller js-top-scroller svg-icons">
         <img alt="Back to Top" src={upArrow} className="scroller-svg" />
-      </a> */}
+      </a>
       <div className='about'>
         <About />
       </div>
