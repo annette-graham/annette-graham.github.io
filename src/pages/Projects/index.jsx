838e937f6e5c6d2f3f40d5c712ff92545a116313
annette-graham <annette.graham@bbc.co.uk>
2020-07-06 10:14:59 +0100

    change wording

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -17,7 +17,7 @@ const Projects = () => {
         <p>This is my project.</p>
         <p>Literally, what else you expect?</p>
         <p>I got nothin else r u happy!?!?!</p>
-        <p>Stop laughing</p>
+        <p>Shut up</p>
         <p>Dick.</p>
         <br />
       </div>
